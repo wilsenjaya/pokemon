46ee624fc85d6791cb150057af1086bc32e7e6fb
wilsen <wilsen.senjaya@mitrais.com>
2022-04-25 00:15:25 +0700

    feat(PWA): [NO-JIRA] Add PWA Support

--- next.config.js
+++ next.config.js
@@ -1,18 +1,34 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa");
+
 const ContentSecurityPolicy = `
   script-src 'self' 'unsafe-eval';
   object-src 'none';
   base-uri 'none';
 `;
 
-const nextConfig = {
+const nextConfig = withPWA({
   reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
   async headers() {
     return [
       {
-        // Apply these headers to all routes in your application.
+        // Apply these headers to all routes in application
         source: '/:path*',
         headers: [
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
           {
             key: 'Content-Security-Policy',
             value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
@@ -25,6 +41,6 @@ const nextConfig = {
   images: {
     domains: ['raw.githubusercontent.com'],
   },
-};
+});
 
 module.exports = nextConfig;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,11 @@ import {
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png" />
+        <meta name="theme-color" content="#fff" />
+      </Head>
       <body>
         <Main />
         <NextScript />
